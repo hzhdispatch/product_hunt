756f93c35ab8ed6fc29ba0dd02df1c5bcaf44fd4
开水白菜 <937370199@qq.com>
2019-08-28 08:55:45 +0800

    增加了登录和退出功能

--- account/views.py
+++ account/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User #导入django自带的用户模板
-
+from django.contrib import auth
 # Create your views here.
 
 def signup(request):
-	if request.method == 'GET':
+	if request.method == 'GET':#Get是到注册页面去
 		return render(request,'signup.html')
-	elif request.method == 'POST':
+	elif request.method == 'POST':#Post是想要注册
 		user_name = request.POST['用户名']#post请求可以从POST字典中获取其包含的内容
 		password1 = request.POST['密码']
 		password2 = request.POST['确认密码']
@@ -20,5 +20,22 @@ def signup(request):
 			else:
 				return render(request,'signup.html',{'密码错误':'两次输入的密码不一致！'})
 
-			
-		
+def login(request):
+	if request.method == 'GET':#Get是到登录页面去
+		return render(request,'login.html')
+	elif request.method == 'POST':#Post是想要登录
+		user_name = request.POST['用户名']
+		pass_word = request.POST['密码']
+		user = auth.authenticate(username=user_name,password=pass_word)#鉴定成功会返回一个用户
+		if user is None:
+			return render(request,'login.html',{'错误':'用户名或密码错误'})
+		else:
+			auth.login(request,user)
+			return redirect('主页')
+
+
+def logout(request):#登出不是get，不从服务器获取资源而是想修改服务器的信息/状态，
+#但html那边默认是一种get—method，需要用隐藏的form标签
+	if request.method == 'POST':#退出就不需要get只要post，一点按钮直接退出就好了
+		auth.logout(request)
+		return redirect('主页') 
